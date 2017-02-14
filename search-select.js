677db6f0189d20da52547503889b67f276a36625
YDJSZY <260211963@qq.com>
2017-02-14 18:11:24 +0800

    commit
    一个能通过服务端获许数据的select

---
+++ search-select.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Apple on 17/2/14.
+ */
+var SearchSelect = function (config) {
+    this.config = config;
+    this.targetEle = null;
+    this.pagination = false;
+    this.render();
+}
+
+SearchSelect.prototype.render = function () {
+    for(var p in this.config) {
+        if(this.config.hasOwnProperty(p)){
+            this[p] = this.config[p];
+        }
+    };
+    var selectContainer = document.createElement("div");
+    selectContainer.setAttribute("class","select-container");
+    var searchInput = document.createElement("input");
+    searchInput.setAttribute("class","search-input");
+    searchInput.onclick = function (e) {
+        var event = e || window.event;
+        event.cancelBubble || event.stopPropagation();
+        searchPanel.style.display = "block";
+    };
+    document.onclick = function (e) {
+        var event = e || window.event;
+        event.cancelBubble || event.stopPropagation();
+        searchPanel.style.display = "none";
+    }
+    selectContainer.appendChild(searchInput);
+    var searchPanel = document.createElement("div");
+    searchPanel.setAttribute("class","search-panel")
+    this.searchPanelUl = document.createElement("ul");
+    this.searchPanelUl.setAttribute("class","search-panel-ul");
+    searchPanel.appendChild(this.searchPanelUl);
+    selectContainer.appendChild(searchPanel);
+    this.targetEle.parentNode.replaceChild(selectContainer,this.targetEle);
+}
+
+SearchSelect.prototype.createLi = function () {
+    var li = document.createElement("li");
+    return li;
+}
+
+SearchSelect.prototype.pushData = function (data) {
+    var frag = document.createDocumentFragment();
+    for(var i = 0,l = data.length; i < l; i++) {
+        var li = this.createLi();
+        li.innerHTML = data[i];
+        frag.appendChild(li)
+    }
+    this.searchPanelUl.appendChild(frag);
+}
